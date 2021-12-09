537cf31bf6c0689fd58ddead615b615c680ac16b
Petrus Repo <petrus.repo@iki.fi>
2021-12-09 12:25:58 +0200

    Wait 30 minutes before starting watcher

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -euo pipefail
 
 cd "${RAPTOREUM_PATH:-/root}" || exit 1
 
 ./runner.sh &
+
+echo "Blindly waiting 30 minutes for raptoreumd to become available during a cold boot"
+sleep 1800
 ./watcher.sh
